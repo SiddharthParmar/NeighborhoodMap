0595658d966ecc91c7d51445cc96a32d89a08d1d
Siddharth Parmar <siddharth989@gmail.com>
2018-01-15 16:05:27 -0800

    Add files via upload

---
+++ js/locations.js
@@ -0,0 +1,47 @@
+//Locations array of hikes around my area
+var locations = [{
+        title: 'Rancho San Antonio',
+        lat: 37.332673,
+        lng: -122.086945
+    },
+    {
+        title: 'Mission Peak',
+        lat: 37.512436,
+        lng: -121.880513
+    },
+    {
+        title: 'Stanford Dish Trail',
+        lat: 37.412748,
+        lng: -122.169268
+    },
+    {
+        title: 'Big Basin',
+        lat: 37.173706,
+        lng: -122.246396
+    },
+    {
+        title: 'Castle Rock',
+        lat: 37.229843,
+        lng: -122.125149
+    },
+    {
+        title: 'Lands End',
+        lat: 37.784884,
+        lng: -122.507510
+    },
+    {
+        title: 'Redwood Regional Park',
+        lat: 37.813875,
+        lng: -122.166090
+    },
+    {
+        title: 'Cataract Falls',
+        lat: 37.931843,
+        lng: -122.635388
+    },
+    {
+        title: 'Marin Headlands',
+        lat: 37.826294,
+        lng: -122.499650
+    }
+]
